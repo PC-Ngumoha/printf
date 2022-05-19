426a39cf5a4537a9ed2770f2cdfc18e34a824de1
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-19 14:12:34 +0000

    Create libmyprint.a

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int large_num = 1000000000;
+
+	_printf("Large Number: %i\n", large_num);
+	printf("Large Number: %i\n", large_num);
+
+	return (0);
+}
