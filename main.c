aa363917191163d2d13261e58f0b885e9f1d429d
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-19 15:47:18 +0000

    Rewrite _printf.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - main entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int large_num = 1000000000;
+
+	_printf("Large Number: [%d, %i]\n", large_num, large_num);
+	printf("Large Number: [%d, %i]\n", large_num, large_num);
+	_printf("The world is waiting for %s\n", "Chukwuemeka");
+	_printf("Give me %c helping hand\n", 'a');
+
+	return (0);
+}
