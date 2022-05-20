6217d0b271dd7b95ece540a9c2403a6c8e6504d2
Nnajinaza <nnajinaza@gmail.com>
2022-05-20 21:07:53 +0000

    Update print_number.c

--- print_unsigned.c
+++ print_unsigned.c
@@ -9,30 +9,8 @@
 int _print_binary(va_list valist)
 {
 	unsigned int num = va_arg(valist, unsigned int);
+	unsigned int i, j;
 
 	print_unsigned(num);
 	return(0);
-}
-
-/**
- * print_nusigned
- * @i: integer parameter
- *
- * Return: Always 0
- */
-void print_unsigned(int i)
-{
-	int j;
-	j = i % 2;
-
-	if (i / 2 && i > 0)
-	{
-		print_unsigned(i / 2);
-		_putchar(j + '0');
-	}
-	if (i < 0)
-	{
-		_putchar('-');
-		_putchar(j + '0');
-	}
 }
