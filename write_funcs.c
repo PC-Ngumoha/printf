010144a7b9df70ac0f5635e41993c74b05f2908e
Nnajinaza <nnajinaza@gmail.com>
2022-05-20 23:22:48 +0000

    Update write_funcs.c

--- write_funcs.c
+++ write_funcs.c
@@ -11,12 +11,11 @@ int _putchar(char c)
 	static char buffer[1024];
 	static int i;
 
-	if (c == -1 || i > 1024)
+	if (c == -1 || i >= 1024)
 	{
-		write(1, &buffer, 1);
+		write(1, &buffer, i);
 		i = 0;
 	}
-
 	if (c != -1)
 	{
 		buffer[i] = c;
