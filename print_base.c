87fbeef8d08b6a560d1a590b0ad98810403a3487
Nnajinaza <nnajinaza@gmail.com>
2022-05-20 21:45:00 +0000

    Create print_base:print_decimal

--- print_base.c
+++ print_base.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _print_binary - function to print unsigned integers
+ * _print_binary - function to print base two numbers
  * @valist: valist parameter
  *
- * Return: Always 0
+ * Return: str array
  */
 int _print_binary(va_list valist)
 {
@@ -16,12 +16,34 @@ int _print_binary(va_list valist)
 	return (_puts(str));
 }
 
+/**
+ * _print_octal - function to print octal numbers
+ * @valist: valist parameter
+ *
+ * Return: str array
+ */
 int _print_octal(va_list valist)
 {
 	unsigned int num = va_arg(valist, unsigned int)
 	char *str;
 	int base = 8;
 
+	ptr = convert(num, base);
+	return (_puts(str));
+}
+
+/**
+ * _print_unsigned
+ * @valist: valist parameter
+ *
+ * Return: str array
+ */
+int _print_unsigned(va_list valist)
+{
+	unsigned int num = va_arg(valist, unsigned int)
+	char *str;
+	int base = 10;
+
 	str = convert(num, base);
 	return (_puts(str));
-}	
+}
