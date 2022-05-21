7a379e7988b61823eaa2fbdbc46e379c08456524
Nnajinaza <nnajinaza@gmail.com>
2022-05-21 09:33:46 +0000

    Update main.h

--- main.h
+++ main.h
@@ -26,7 +26,6 @@ int _printf(const char *format, ...);
 /* Print character functions */
 int _print_char(va_list);
 int _print_str(va_list);
-int _print_percent(va_list);
 
 /* Print numbers in decimal */
 int  _print_dec(va_list);
