d17a2b34033d7e6337260712e9c69105420f1611
Nnajinaza <nnajinaza@gmail.com>
2022-05-21 16:12:56 +0000

    Create custom conversion specifier

--- print_letters.c
+++ print_letters.c
@@ -27,3 +27,29 @@ int _print_str(va_list valist)
 
 	return (_puts(str));
 }
+
+int _print_specifier(va_list valist)
+{
+	int i;
+	int count;
+	char *str = va_arg(valist, char *);
+
+	if (str == NULL)
+		str = "(null)";
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			count = count + 2;
+		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
+}
