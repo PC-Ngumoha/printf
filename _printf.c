6ea9876e1dadd42dba67940eb13ce8a7b9b5bbd1
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-22 10:14:03 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -8,13 +8,18 @@
  */
 int _printf(const char *format, ...)
 {
-	register int count = 0;
 	va_list args;
 	const char *str;
 	int (*func)(va_list, flag_t *);
 	flag_t f = {0, 0, 0};
+	
+	register int count = 0;
 
 	va_start(args, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	for (str = format; *str; str++)
 	{
 		if (*str == '%')
@@ -28,12 +33,10 @@ int _printf(const char *format, ...)
 			while (get_flags(*str, &f))
 				str++;
 			func = get_func(*str);
-			count += func(args, &f);
+			count += (func) ? func(args, &f) : _printf("%%%c", *str);
 		}
 		else
-		{
 			count += _putchar(*str);
-		}
 	}
 	_putchar(-1);
 	va_end(args);
